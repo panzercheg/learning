c213137c0d7d43cff0a81e3b0ef96061b1dfade5
r.burenkov <r.burenkov@corp.mail.ru>
2017-11-02 15:07:56 +0300

    week2: draft storage.py 2

--- week_2/storage.py
+++ week_2/storage.py
@@ -8,5 +8,12 @@ def main():
     parser.add_argument('-v', '--val', action='store', dest='val_name', help='Value to store')
     options = parser.parse_args()
 
+    if options.key_name and options.val_name:
+        print('Store value in storage by key:{key1} and value:{val1}'.format(key1=options.key_name,val1=options.val_name))
+
+    if options.key_name and not options.val_name:
+        print('Get key _ {key1} _ from storage '.format(key1=options.key_name))
+
+
 if __name__ == '__main__':
     main()
