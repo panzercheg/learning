e9050429a92e8ee007a3bec9b7429fb65f2ea997
panzercheg <33318274+panzercheg@users.noreply.github.com>
2017-11-02 22:26:11 +0300

    week_2: draft 3

--- week_2/storage.py
+++ week_2/storage.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import argparse
+import json
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,10 +9,22 @@ def main():
     parser.add_argument('-v', '--val', action='store', dest='val_name', help='Value to store')
     options = parser.parse_args()
 
+    dict = {}
+    # options.key_name = key
+    # options.val_name = val
+
+    print(tempfile.gettempdir())
+    file_json = tempfile.NamedTemporaryFile
+    # storage_path = os.path.join(tempfile.gettempdir(), 'storage.json')
+    with open(tempfile.gettempdir(), 'r+', encoding="utf-8") as file:
+        json.dump(dict, file)
+
     if options.key_name and options.val_name:
+        dict.fromkeys([options.key_name],options.val_name)
         print('Store value in storage by key:{key1} and value:{val1}'.format(key1=options.key_name,val1=options.val_name))
 
     if options.key_name and not options.val_name:
+        print(dict.get(options.key_name))
         print('Get key _ {key1} _ from storage '.format(key1=options.key_name))
 
 
