59d5dbf307567faa011946f9b472664f4bfc3681
r.burenkov <r.burenkov@corp.mail.ru>
2017-11-09 09:39:38 +0300

    week_2: draft 3

--- week_2/storage.py
+++ week_2/storage.py
@@ -9,24 +9,21 @@ def main():
     parser.add_argument('-v', '--val', action='store', dest='val_name', help='Value to store')
     options = parser.parse_args()
 
-    dict = {}
-    # options.key_name = key
-    # options.val_name = val
 
-    print(tempfile.gettempdir())
-    file_json = tempfile.NamedTemporaryFile
-    # storage_path = os.path.join(tempfile.gettempdir(), 'storage.json')
-    with open(tempfile.gettempdir(), 'r+', encoding="utf-8") as file:
-        json.dump(dict, file)
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    print(storage_path)
+    with open(storage_path, 'w+') as file_storage:
+        if options.key_name and options.val_name:
+            my_dict = dict.fromkeys([options.key_name], options.val_name)
+            file_storage.write(json.dumps(my_dict))
+            print('Store value in storage by key:{key1} and value:{val1}'.format(key1=options.key_name,
+                                                                                 val1=options.val_name))
 
-    if options.key_name and options.val_name:
-        dict.fromkeys([options.key_name],options.val_name)
-        print('Store value in storage by key:{key1} and value:{val1}'.format(key1=options.key_name,val1=options.val_name))
-
-    if options.key_name and not options.val_name:
-        print(dict.get(options.key_name))
-        print('Get key _ {key1} _ from storage '.format(key1=options.key_name))
-
+        if options.key_name and not options.val_name:
+            print(file_storage.read())
+            my_dict = json.loads(file_storage.read())
+            print(my_dict.get(options.key_name))
+            print('Get key _ {key1} _ from storage '.format(key1=options.key_name))
 
 if __name__ == '__main__':
     main()
