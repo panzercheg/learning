772517cf8c06d9134a8b176400c315fe79bb817d
panzercheg <33318274+panzercheg@users.noreply.github.com>
2018-01-03 18:46:38 +0300

    Week_2
    improvements fo decorator

--- week_2/decorator2.py
+++ week_2/decorator2.py
@@ -5,11 +5,18 @@ from functools import wraps
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
+
 def to_json(func):
     @wraps(func)
     def to_json_wrapped(*args, **kwargs):
         result = func(*args, **kwargs)
-        with open(storage_path, 'w') as json_file:
-            json_file.write(json.dumps(result))
-        return result
+        if result is None:
+            result = 'null'
+            with open(storage_path, 'w') as json_file:
+                json_file.write(json.dumps(result))
+            return result
+        else:
+            with open(storage_path, 'w') as json_file:
+                json_file.write(json.dumps(json.dumps(result)))
+            return result
     return to_json_wrapped
--- week_2/decorator_to_json.py
+++ week_2/decorator_to_json.py
@@ -5,26 +5,36 @@ from functools import wraps
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
+
 def to_json(func):
     @wraps(func)
     def to_json_wrapped(*args, **kwargs):
         result = func(*args, **kwargs)
-        with open(storage_path, 'w') as json_file:
-            json_file.write(json.dumps(result))
-        return result
+        if result is None:
+            result = 'null'
+            with open(storage_path, 'w') as json_file:
+                json_file.write(json.dumps(result))
+            return result
+        else:
+            with open(storage_path, 'w') as json_file:
+                json.dumps(result)
+                json_file.write(json.dumps(json.dumps(result)))
+            return result
     return to_json_wrapped
+
 
 @to_json
 def decorated(num_list):
-    return {
-        # 'data': 88
-        None
-    }
+    return num_list
 
-# print(decorated([1,2,7,9,2]))
+decorated("")
+
+
+# print(decorated([1,2,3,9,2]))
+# print(decorated([""]))
 
 with open(storage_path, 'r') as json_file:
-    print(json_file.read())
-    # print(storage_path.format(json_file.read()))
+    print("Читаем файл json " + json_file.read())
+    print("форматируем файл в тру-json " + storage_path.format(json_file.read()))
 
-print(decorated.__name__)
+print("имя декорируемой функции " + decorated.__name__)
