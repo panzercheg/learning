3c7cd6eee47a76aa92e2e63ee5dd1d7a669c5a2b
r.burenkov <r.burenkov@corp.mail.ru>
2018-01-31 14:32:05 +0300

    Desc with commision

--- week_4/desc_commission.py
+++ week_4/desc_commission.py
@@ -2,13 +2,13 @@ class Value:
     def __init__(self):
         self.value = None
 
-    def __set__(self, commission, value):
-        self.value = value - value * 0.5
+    def __set__(self, instance, value):
+        self.value = value - value * instance.commission
 
-    def __get__(self, commission, value):
+    def __get__(self, instance, value):
         return self.value
 
-    def __delete__(self, commission):
+    def __delete__(self, instance):
         print("delete value obj")
 
 
@@ -19,7 +19,8 @@ class Account:
         self.commission = commission
 
 
-new_account = Account(0.1)
+
+new_account = Account(0.5)
 new_account.amount = 100
 
 print(new_account.amount)
