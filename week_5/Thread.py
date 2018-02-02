6a116ddca17304cc524a331e6e7a9fc240776f0d
r.burenkov <r.burenkov@corp.mail.ru>
2018-02-02 09:07:38 +0300

    week_5: Thread stuff

---
+++ week_5/Thread.py
@@ -0,0 +1,8 @@
+from threading import Thread
+
+def f(name):
+    print("hello", name)
+
+th = Thread(target=f, args=("Bob",))
+th.start()
+th.join()
---
+++ week_5/ThreadClass.py
@@ -0,0 +1,13 @@
+from threading import Thread
+
+class PrintThread(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        print("hello", self.name)
+
+th = PrintThread("Mike")
+th.start()
+th.join()
