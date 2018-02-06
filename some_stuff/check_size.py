98c9bfffeb404e71e8ddce0c9a85250483f45101
r.burenkov <r.burenkov@corp.mail.ru>
2018-02-06 16:10:05 +0300

    CheckSize: little rework

--- some_stuff/check_size.py
+++ some_stuff/check_size.py
@@ -13,6 +13,23 @@ def comparison_size(i, j, action):
 
 
 def parse_args():
+    def one_file(file_ext, path_to_file):
+        size_byte = os.path.getsize(os.path.join(path_to_file))
+        size_mb = size_byte / (1024 * 1024.0)
+        size_mb = round(size_mb, 2)
+        if path_to_file and not file_ext:
+            check = os.path.isfile(path_to_file)
+            if check is False:
+                print('[Warning] file {} doesn`t exists!'.format(path_to_file))
+            if check is True:
+                file_size = comparison_size(int(threshold), size_mb, sign)
+                if file_size is False:
+                    print('[Warning] Size file {} is {} MB and '
+                          'file must be {} threshold {} MB'.
+                          format(path_to_file, size_mb, sign, str(threshold), ))
+                else:
+                    print('Size file {} is ok {} MB'.format(path_to_file, size_mb))
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', action='store', dest='file_path', help='Path for check size')
     parser.add_argument('-f', '--file', action='store', dest='file_write', help='Path for output file', default=False)
@@ -30,8 +47,8 @@ def parse_args():
     threshold = options.file_threshold
     sign = options.sign_check
 
-    print('File size must be {sign} threshold {threshold} MB'.
-          format(file=path_to_file, threshold=str(threshold), sign=sign))
+    one_file(file_ext, path_to_file)
+
     for root, dirs, files in os.walk(path_to_file):
         for file in files:
             if file.endswith(file_ext):
@@ -41,27 +58,25 @@ def parse_args():
                 size_mb = round(size_mb, 2)
                 if not path_to_write:
                     without_write = comparison_size(int(threshold), size_mb, sign)
-                    if without_write is True:
-                        print('[Warning] Size file {file} is {size} MB and '
-                              'file must be {sign} threshold {threshold} MB'.
-                              format(file=path_to_file, size=size_mb, threshold=str(threshold), sign=sign))
-                    elif path_to_write is False:
-                        print('Size file {file} is ok {size} MB'.format(file=path_to_file, size=size_mb))
+                    if without_write is False:
+                        print('[Warning] Size file {} is {} MB and '
+                              'file must be {} threshold {} MB'.
+                              format(path_to_file, size_mb, sign, str(threshold), ))
                     else:
-                        print('Size file {file} is ok {size} MB'.format(file=path_to_file, size=size_mb))
+                        print('Size file {} is ok {} MB'.format(path_to_file, size_mb))
                 if path_to_write:
                     with open(path_to_write, 'a') as data:
                         with_write = comparison_size(int(threshold), size_mb, sign)
                         if with_write is False:
                             print('[Warning]'
-                                  ' Size file {file} is {size} MB and file must be {sign} threshold {threshold} MB'.
-                                  format(file=path_to_file, size=size_mb, threshold=str(threshold), sign=sign))
+                                  ' Size file {} is {} MB and file must be {} threshold {} MB'.
+                                  format(path_to_file, size_mb, sign, str(threshold)))
                             data.write('\n[Warning].'
-                                       'Size file {file} is {size} MB and file must be {sign} threshold {threshold} MB'.
-                                       format(file=path_to_file, size=size_mb, threshold=str(threshold), sign=sign))
+                                       'Size file {} is {} MB and file must be {} threshold {} MB'.
+                                       format(path_to_file, size_mb, sign, str(threshold)))
                         else:
-                            print('Size file {file} is ok {size} MB'.format(file=path_to_file, size=size_mb))
-                            data.write('\nSize file {file} is ok {size} MB'.format(file=path_to_file, size=size_mb))
+                            print('Size file {} is ok {} MB'.format(path_to_file, size_mb))
+                            data.write('\nSize file {} is ok {} MB'.format(path_to_file, size_mb))
 
 
 if __name__ == "__main__":
