d76cc8762f29e059ed8f28df6f6e9fd06221e211
r.burenkov <r.burenkov@corp.mail.ru>
2018-11-07 14:30:52 +0300

    add few features for commit log notify: add support few branches from different repos. Add config for since/until log. Dedicated email for every repos in mail-name

--- git_log_by_author/commit_log_notify.py
+++ git_log_by_author/commit_log_notify.py
@@ -5,6 +5,9 @@ import os
 import pprint
 import datetime
 import sys
+import json
+import argparse
+import re
 
 sys.path.append(os.getcwd())
 
@@ -12,13 +15,33 @@ from some_stuff.mail_tools import MailTools
 from some_stuff.file_actions import FileActions
 
 
-qa_list_templates = {
-    'prefix_qa_for_send_mail': ['git author'] # 'j.smith': ['j.jonson', 'b.pitt', 't.hardy']
-}
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q', '--qa', action='store', dest='qa_list', help='List QA for checkers')
+    parser.add_argument('-r', '--repo', action='store', dest='repo', help='Path to repo')
+    parser.add_argument('-s', '--since', action='store', dest='since', help='date since')
+    parser.add_argument('-u', '--until', action='store', dest='until', help='date until')
+    options = parser.parse_args()
 
-now = datetime.datetime.now()
-curr_date = now.strftime("%d-%m-%Y")
-mail_subject = "Daily commit {}".format(curr_date)
+    return options
+
+
+def read_config(config):
+    with open(config) as json_file:
+        result = json.loads(json_file.read())
+        return result
+
+
+args = parse_args()
+qa_list_templates = read_config(args.qa_list)
+
+
+def mail_name(repo_name):
+    now = datetime.datetime.now()
+    curr_date = now.strftime("%d-%m-%Y")
+    mail_subject = "Daily commit {} from repo {}".format(curr_date, repo_name)
+
+    return mail_subject
 
 new_list = set()
 for value in qa_list_templates.values():
@@ -32,16 +55,16 @@ def mail_log(address, subject, message):
     mail.send()
 
 
-def get_git_log(repo):
+def get_git_log(repo, since, until, repo_name):
     res = []
     for key, value in qa_list_templates.items():
         res += value
     good_list = '|'.join(res)
     gitlog = FileActions().call('git log origin/master --pretty=format:"%an;;;%ai'
-                                ' %s https://your_gitlab_url/commit/%H"'
+                                ' %s https://your_gitlab_url/{}/commit/%H"'
                                 ' --author="{}"'
-                                ' --since="yesterday 07:00"'
-                                ' --until="today 07:00"'.format(good_list),
+                                ' --since="{}"'
+                                ' --until="{}"'.format(repo_name, good_list, since, until),
                                 cd=repo,
                                 return_output=True,
                                 silent=True, output_encoding="utf-8")
@@ -62,15 +85,20 @@ def parse_git_data(parse_gitlog):
 
     return globout
 
-
-sys.stdout = open(1, 'w', encoding='utf-8', closefd=False) # for right output from pprint(out)
+sys.stdout = open(1, 'w', encoding='utf-8', closefd=False)
 
 if __name__ == '__main__':
-    repo = sys.argv[1]
-    FileActions().call('git fetch origin', cd=repo, return_output=True, silent=True)
-    parse_gitlog = get_git_log(repo)
+    args = parse_args()
+    print(args.qa_list)
+    FileActions().call('git fetch origin', cd=args.repo, return_output=True, silent=True)
+    FileActions().call('git config --global grep.extendedRegexp true', cd=args.repo, return_output=True, silent=True)
+    (_, repo_name, _) = FileActions().call('git config --get remote.origin.url',
+                                           cd=args.repo, return_output=True, silent=True)
+    result = re.findall(r't1/(.*)\.', repo_name)
+    result = ''.join(result)
+    parse_gitlog = get_git_log(args.repo, args.since, args.until, result)
     out = parse_git_data(parse_gitlog)
     for key, value in out.items():
         pure_value = '\n'.join(value)
-        mail_log(key, mail_subject, pure_value)
+        mail_log(key, mail_name(result), pure_value)
     pprint.pprint(out)
